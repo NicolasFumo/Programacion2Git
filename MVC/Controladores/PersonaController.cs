9528450fe80dafe7af93871b46166c43d2fe9716
Nicolas <nicolasfumo@gmail.com>
2023-04-24 21:17:10 -0300

    Agregado Arquitectura MVC

---
+++ MVC/Controladores/PersonaController.cs
@@ -0,0 +1,29 @@
+﻿using MVC.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC.Controladores
+{
+    public class PersonaController
+    {
+        private Repositorio Repositorio;
+
+        public PersonaController()
+        {
+            Repositorio = new Repositorio();
+        }
+
+        public void RegistrarPersona(Persona p)
+        {
+            Repositorio.Personas.Add(p);
+        }
+
+        public List<Persona> ObtenerTodos()
+        {
+            return Repositorio.Personas;
+        }
+    }
+}
---
+++ MVC/Modelos/Repositorio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC.Modelos
+{
+    public class Repositorio
+    {
+        public List<Persona> Personas { get; set; }
+
+        public Repositorio()
+        {
+            Personas = new List<Persona>();
+        }
+    }
+}
